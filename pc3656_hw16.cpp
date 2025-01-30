9a808f74fa15aa47e89cb5f07a4a3605770995dc
Pierre Coric <pierrecoric@Elizas-MBP.lan>
2025-01-30 14:42:56 +0100

    copy constructor

--- pc3656_hw16.cpp
+++ pc3656_hw16.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 class Frame;
 class Stack;
-
 typedef Frame* framePtr;
 
+//Frame class.
 class Frame {
     private:
         char data;
@@ -23,13 +23,12 @@ class Frame {
         Frame(const Frame& f);
         //Destructor
         ~Frame(){};
-
+        //Accessors and Mutators.
         char getData() {return data;}
         void setData(char c) {data = c;}
         framePtr getNext() {return next;}
-
+        //Overloading the = operator.
         Frame operator =(const Frame& other);
-
         friend class Stack;
 };
 
@@ -39,7 +38,7 @@ Frame::Frame(const Frame& f) {
     next = f.next;
 }
 
-//Overloading = operator.
+//Overloading = operator for the Frame class..
 Frame Frame::operator =(const Frame& other) {
     if(this != &other) {
         data = other.data;
@@ -48,51 +47,91 @@ Frame Frame::operator =(const Frame& other) {
     return *this;
 }
 
+//Stack class
 class Stack {
     private:
+        int size;
         framePtr top;
     public:
         //Simple constructor.
-        Stack() : top(nullptr) {};
+        Stack() : top(nullptr), size(0) {};
         //Copy constructor definition.
         Stack(const Stack& original);
         //Destructor.
         //~Stack() {clear();}
+        //void clear();
+        bool empty() const {return size == 0;}
         //Push a new frame.
         void push(framePtr& f);
         //Push a char directly.
         void push(char c);
         framePtr pop();
-        //void clear();
         void print() const;
         //Overloading <<.
         friend ostream& operator <<(ostream& outs, const Stack& s);
+        friend istream& operator >>(istream& ins, Stack& s);
         friend bool pascalCheck(const Stack& s);
         friend class Frame;
 };
 
+//Copy constructor.
+Stack::Stack(const Stack& original) {
+    //Initialization.
+    top = nullptr;
+    size = 0;
+    //Make sure the original is not empty.
+    if(!original.empty()){
+        //Copy the top frame.
+        framePtr oldPtr = original.top;
+        top = new Frame(oldPtr->data);
+        top -> next = nullptr;
+
+        //Copy the rest.
+        framePtr nextOne = top;
+        oldPtr = oldPtr -> next;
+
+        while(oldPtr != nullptr) {
+            framePtr newOne = new Frame(oldPtr -> data);
+            newOne -> next = nullptr;
+            nextOne -> next = newOne;
+
+            //Move to the next node.
+            nextOne = nextOne -> next;
+            oldPtr = oldPtr -> next;
+        }
+    }
+}
+
+//Clear function.
+
+//Pushing a new frame.
 void Stack::push(framePtr& f) {
     //Pushing on an empty stack.
     if(top == nullptr) {
         top = f;
+        size ++;
         return;
     }
     f -> next = top;
     top = f;
+    size ++;
 }
 
+//Pushing a character.
 void Stack::push(char c) {
     framePtr f = new Frame(c);
     //Pushing on an empty stack.
     if(top == nullptr) {
         top = f;
+        size ++;
         return;
     }
     f -> next = top;
     top = f;
+    size ++;
 }
 
-
+//Poping from the stack.
 
 //Function to print the stack.
 void Stack::print() const {
@@ -103,7 +142,7 @@ void Stack::print() const {
     }
 }
 
-//Overloading <<.
+//Overloading << for the Stack class.
 ostream& operator <<(ostream& outs, const Stack& s) {
     framePtr temp = s.top;
     while(temp != nullptr) {
@@ -113,6 +152,15 @@ ostream& operator <<(ostream& outs, const Stack& s) {
     return outs;
 }
 
+//Overloading >> for the Stack class.
+istream& operator >>(istream& ins, Stack& s) {
+    char c;
+    ins >> c;
+    s.push(c);
+    return ins;
+}
+
+//Main function.
 int main() {
     Stack s;
     framePtr f = new Frame('X');
@@ -123,5 +171,12 @@ int main() {
     s.push('p');
     s.push('e');
     cout << endl << s << endl;
+    char ans('\0');
+    cin >> s;
+    cin >> s;
+    cin >> s;
+    cout << endl << s << endl;
+    Stack t(s);
+    cout << endl << t << endl;
     return 0;
 }
